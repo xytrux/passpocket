495a8f49fadbdbb79c349a6320514c59b65dd19c
ClaytonTDM <claytontdm@gmail.com>
2024-06-21 23:55:38 -0400

    fix icon 2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function createWindow() {
     let win = new BrowserWindow({
         width: 800,
         height: 600,
-        icon: "icon.png",
+        icon: path.join(__dirname, "icon", "icon.png"),
         autoHideMenuBar: true,
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),

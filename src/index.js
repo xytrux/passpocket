8f7585e52515d6314709a344dc25330d84db5e00
ClaytonTDM <claytontdm@gmail.com>
2024-06-21 23:54:29 -0400

    fix location

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function createWindow() {
         },
     });
 
-    win.loadFile("../index.html");
+    win.loadFile(path.join(__dirname, "..", "index.html"));
 }
 app.commandLine.appendSwitch("--enable-features", "FluentScrollbar");
 
